74240dda068c12de3ee37e40eaa0379212f27ef2
Geisabitt <ageisislane@gmail.com>
2022-12-10 10:49:37 -0300

    NavBar Mobile addEventListener em todos os links

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 const buttonMobile = document.getElementById('buttonMobile');
+const btnGaleria = document.getElementById('btnGaleria');
+const btnContato = document.getElementById('btnContato');
+const btnInscreva = document.getElementById('btnInscreva');
 
 function toggleMenu() {
     
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
-
 }
 buttonMobile.addEventListener('click', toggleMenu)
+btnGaleria.addEventListener('click', toggleMenu)
+btnContato.addEventListener('click', toggleMenu)
+btnInscreva.addEventListener('click', toggleMenu)
