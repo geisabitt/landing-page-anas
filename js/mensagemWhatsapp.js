81367298cc61b5cb71f2e4e358e044be5fc2b4f7
Geisabitt <ageisislane@gmail.com>
2022-12-31 19:32:57 -0300

    alteração na navbar, alterações de estruturais nas classes, versão mobile completa e pronta para testes.♥

--- js/mensagemWhatsapp.js
+++ js/mensagemWhatsapp.js
@@ -1,17 +1,26 @@
-function enviarMensagem(){
+function enviarMensagem() {
+  var nome = document.querySelector("#nome").value;
+  nome = window.encodeURIComponent(nome);
 
-    var nome = document.querySelector("#nome").value;
-    nome = window.encodeURIComponent(nome);
+  var celular = document.querySelector("#celular").value;
+  celular = celular.replace(/\D/g, "");
 
-    var celular = document.querySelector("#celular").value;
-    celular = celular.replace(/\D/g,'');
+  var mensagem = document.querySelector("#mensagem").value;
+  mensagem = window.encodeURIComponent(mensagem);
 
-    var mensagem = document.querySelector("#mensagem").value;
-    mensagem = window.encodeURIComponent(mensagem);
-  
-    let urlApi = "https://api.whatsapp.com/send?phone=5521982708329&text=";
+  let urlApi = "https://api.whatsapp.com/send?phone=5521982708329&text=";
 
-
-  window.open(urlApi + "Nome:%20" + nome  + "%0d%0a" + "Telefone:%20" + celular + "%0d%0a" + "Mensagem:%20" + mensagem, "_blank");
-	//Obs.. use "_system", no lugar de blank, caso você esteja usando Phonegap / Cordova / Ionic ou qualquer um baseado em webview;
+  window.open(
+    urlApi +
+      "Nome:%20" +
+      nome +
+      "%0d%0a" +
+      "Telefone:%20" +
+      celular +
+      "%0d%0a" +
+      "Mensagem:%20" +
+      mensagem,
+    "_blank"
+  );
+  //Obs.. use "_system", no lugar de blank, caso você esteja usando Phonegap / Cordova / Ionic ou qualquer um baseado em webview;
 }
